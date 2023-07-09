ed82751372676113987e23874f601f9c749f53d6
md ranit rubbyat sultan <mdranitrubbyatsultan@gmail.com>
2023-07-09 14:48:03 +0600

    some modify

--- src/pages/blog page/MainBlog/MainBlog.jsx
+++ src/pages/blog page/MainBlog/MainBlog.jsx
@@ -14,13 +14,14 @@ const MainBlog = () => {
                 {/* map will be apply */}
 
                 <div className='m-5 p-6 blog-container'>
-                    <h3 className='text-center text-3xl font-bold pb-5 blog-head'>blog-1</h3>
-                    <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ut id officiis qui mollitia atque possimus fuga ipsa eaque unde, aliquam ullam? Laborum animi ut autem repellat dicta, et nihil?</p>
+                    <h3 className='text-center text-3xl font-bold pb-5 blog-head'>Happy Birth Day To You Keya</h3>
+                    <p className='text-lg'>Hey! Many many happy returns of the day...</p>
                 </div>
 
             </div>
+            {/* user.email === "mdranitrubbyatsultan@gmail.com" */}
             {
-                user.email === "mdranitrubbyatsultan@gmail.com" ?
+                user.email ?
                     <BlogPost></BlogPost>
                     :
                     ''
